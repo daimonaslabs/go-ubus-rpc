fb050b64c98867499c7f65093fb86042b187d8a4
jrodonnell <james.riley.odonnell@gmail.com>
2025-03-18 18:19:12 -0600

    add UbusBool type

--- types.go
+++ types.go
@@ -6,3 +6,24 @@ type UCIConfigOptionsStatic struct {
 	Name      string `json:"name" ubus:".name"`
 	Index     int    `json:"index" ubus:".index"`
 }
+
+var UbusBoolFalse = "0"
+var UbusBoolTrue = "1"
+
+type UbusBool string
+
+func ConvertBool(b bool) string {
+	if b {
+		return UbusBoolTrue
+	} else {
+		return UbusBoolFalse
+	}
+}
+
+func True() string {
+	return UbusBoolTrue
+}
+
+func False() string {
+	return UbusBoolFalse
+}
