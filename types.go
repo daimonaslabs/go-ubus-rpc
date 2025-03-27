39680caaf1de8252797e67527670584cd88d6641
jrodonnell <james.riley.odonnell@gmail.com>
2025-03-26 21:48:15 -0600

    add more wrapper types to play nicer with ubus

--- types.go
+++ types.go
@@ -1,5 +1,11 @@
 package v1alpha1
 
+import (
+	"net"
+	"net/netip"
+	"time"
+)
+
 type UCIConfigOptionsStatic struct {
 	Anonymous bool   `json:"dotAnonymous" ubus:".anonymous"`
 	Type      string `json:"dotType" ubus:".type"`
@@ -26,4 +32,66 @@ func True() string {
 
 func False() string {
 	return UbusBoolFalse
+}
+
+type IP struct {
+	netip.Addr
+}
+
+// This DeepCopyInto is a manually created deepcopy function, copying the receiver, writing into out.
+// It must be non-nil.
+func (in *IP) DeepCopyInto(out *IP) {
+	*out = *in
+}
+
+// This DeepCopy is a manually created deepcopy function, copying the receiver, creating a new IP.
+func (in *IP) DeepCopy() *IP {
+	if in == nil {
+		return nil
+	}
+	out := new(IP)
+	in.DeepCopyInto(out)
+	return out
+}
+
+type MAC struct {
+	net.HardwareAddr
+}
+
+// This DeepCopyInto is a manually created deepcopy function, copying the receiver, writing into out.
+// It must be non-nil.
+func (in *MAC) DeepCopyInto(out *MAC) {
+	*out = *in
+}
+
+// This DeepCopy is a manually created deepcopy function, copying the receiver, creating a new MAC.
+func (in *MAC) DeepCopy() *MAC {
+	if in == nil {
+		return nil
+	}
+	out := new(MAC)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// TODO make this safer, splitu into separate types which only contain required values
+// see FirewallConfig.RuleSection for all the different time and date options needed
+type Time struct {
+	time.Time
+}
+
+// This DeepCopyInto is a manually created deepcopy function, copying the receiver, writing into out.
+// It must be non-nil.
+func (in *Time) DeepCopyInto(out *Time) {
+	*out = *in
+}
+
+// This DeepCopy is a manually created deepcopy function, copying the receiver, creating a new Time.
+func (in *Time) DeepCopy() *Time {
+	if in == nil {
+		return nil
+	}
+	out := new(Time)
+	in.DeepCopyInto(out)
+	return out
 }
