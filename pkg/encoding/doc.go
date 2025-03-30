d97a488733ad41013c5608b34ef0b8d67c9350e3
jrodonnell <james.riley.odonnell@gmail.com>
2025-03-30 02:22:12 -0600

    add some *Config types to encoding package

---
+++ pkg/encoding/doc.go
@@ -0,0 +1,6 @@
+/*
+This package contains types and functions which make it easy to construct ubus calls by providing type
+safety to the unique ubus parameters as well as providing a new ubus encoder so that standard Go structs
+can be marshaled into these types.
+*/
+package encoding
