dae92c007f1056a6a8525f9a9265fbf212bb7ec5
jrodonnell <james.riley.odonnell@gmail.com>
2025-04-04 01:59:36 -0600

    get basic session login lib working

--- cmd/go-ubus-rpc/main.go
+++ cmd/go-ubus-rpc/main.go
@@ -1,47 +1,13 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-	"time"
 
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/daimonaslabs/go-ubus-rpc/pkg/client"
 )
 
-type Params []any
-
-func NewInitSessionRequest(username, password string) Params {
-	return []any{
-		"00000000000000000000000000000000",
-		"session",
-		"login",
-		map[string]string{
-			"username": username,
-			"password": password,
-		},
-	}
-}
-
 func main() {
-	tokenHeader := rpc.WithHeader("Content-Type", "application/json")
-	httpClient := rpc.WithHTTPClient(&http.Client{
-		Timeout: 10 * time.Second,
-	})
-	ctx := context.Background()
-	client, err := rpc.DialOptions(ctx, "http://10.0.0.1/ubus", httpClient, tokenHeader)
-	response := []any{}
-	request := NewInitSessionRequest("root", "D@!monas")
-	requestJSON, err := json.Marshal(request)
-	err = client.CallContext(ctx, &response, "call", request...)
-	if err != nil {
-		fmt.Println("err: ", err)
-		log.Println("Error initiating ubus RPC session")
-	}
-	fmt.Println("request: ", request)
-	fmt.Println("requestJSON: ", string(requestJSON))
-	fmt.Println("response: ", response)
+	sesh := client.NewSession("root", "D@!monas", "http://10.0.0.1/ubus")
 
+	fmt.Println("sesh: ", sesh)
 }
