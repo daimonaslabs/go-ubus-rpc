4dd74f190c4f0ac5a7c0f9343b9ca0538e2c71e0
jrodonnell <james.riley.odonnell@gmail.com>
2025-06-23 22:57:13 -0600

    add license blurb

--- pkg/client/uci.go
+++ pkg/client/uci.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 Daimonas Labs.
+
+Licensed under the GNU General Public License, Version 3 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.gnu.org/licenses/gpl-3.0.en.html
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package client
 
 import (
@@ -240,47 +256,47 @@ func (opts UCIGetOptions) GetResult(p Response) (u UCIGetResult, err error) {
 			for _, section := range obj.Values {
 				switch s := section.(type) {
 				case dhcp.BootSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.CircuitIDSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.DHCPSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.DnsmasqSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.HostSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.HostRecordSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.MACSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.OdhcpdSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.RelaySection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.RemoteIDSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.SubscrIDSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.TagSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.UserClassSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case dhcp.VendorClassSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case firewall.DefaultsSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case firewall.ForwardingSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case firewall.IPSetSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case firewall.IncludeSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case firewall.RedirectSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case firewall.RuleSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				case firewall.ZoneSection:
-					u.SectionArray = append(u.SectionArray, s)
+					u.Sections = append(u.Sections, s)
 				}
 			}
 		default:
@@ -344,7 +360,7 @@ type UCIConfigsResult struct {
 type UCIGetResult struct {
 	// if any combination of Config, Section, and Type are specified, return a set of
 	// ConfigSection(s)
-	SectionArray []uci.ConfigSection `json:"sectionArray,omitempty"`
+	Sections []uci.ConfigSection `json:"sections,omitempty"`
 	// if Option is set in UCIGetOptions, return a single option's value
 	Option map[string]uci.List `json:"option,omitempty"`
 }
